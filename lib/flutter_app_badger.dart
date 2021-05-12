87982cb481fe6d3c5a5cc0894ee6c78697b556a9
Brad Chien <bradchien@kktv.me>
2021-05-12 14:15:21 +0800

    async method call

--- lib/flutter_app_badger.dart
+++ lib/flutter_app_badger.dart
@@ -3,19 +3,20 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class FlutterAppBadger {
-  static const MethodChannel _channel =
-      const MethodChannel('g123k/flutter_app_badger');
+	static const MethodChannel _channel =
+	const MethodChannel('g123k/flutter_app_badger');
 
-  static void updateBadgeCount(int count) {
-    _channel.invokeMethod('updateBadgeCount', {"count": count});
-  }
+	static Future<void> updateBadgeCount(int count) async {
+		_channel.invokeMethod('updateBadgeCount', {"count": count});
+	}
 
-  static void removeBadge() {
-    _channel.invokeMethod('removeBadge');
-  }
+	static Future<void> removeBadge() async {
+		_channel.invokeMethod('removeBadge');
+	}
 
-  static Future<bool> isAppBadgeSupported() async {
-    bool? appBadgeSupported = await _channel.invokeMethod('isAppBadgeSupported');
-    return appBadgeSupported ?? false;
-  }
+	static Future<bool> isAppBadgeSupported() async {
+		bool? appBadgeSupported = await _channel.invokeMethod(
+				'isAppBadgeSupported');
+		return appBadgeSupported ?? false;
+	}
 }
